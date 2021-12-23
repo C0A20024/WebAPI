a398813e4b8bfead67862ee17d2c7a9c816918ec
C0A20024 <68372680+C0A20024@users.noreply.github.com>
2021-12-23 17:48:23 +0900

    Update member.js

--- member.js
+++ member.js
@@ -2,29 +2,9 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-app.get("/members", (req,res) => {
-	  const members = [
-		    {
-			        id: c0a20024,
-			        name: "内山幸大",
-			      },
-		    {
-			        id: 2,
-			        name: "screwdriver",
-			      },
-		    ,
-		    {
-			        id: 3,
-			        name: "wrench",
-			      },
-        
-        {
-			        id: 4,
-			        name: "wrench",
-			      },
-		   ];
-
-	 res.json(members);
-});
+app.get('/', (req, res) => res.send('Hello World!'));
+app.get('/', (req, res) => res.send('Hello World!'));
+app.get('/', (req, res) => res.send('Hello World!'));
+app.get('/', (req, res) => res.send('Hello World!'));
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
